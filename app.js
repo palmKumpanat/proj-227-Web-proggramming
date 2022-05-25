250ebdd80b0e05297516aed273259083eda2d578
palmKumpanat <kumpanat.joompabuth@gmail.com>
2022-05-25 17:33:51 +0700

    PalmShop

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.static("./public"));
 app.use(bodyParser.urlencoded({extened: true}));
 app.use(methodOverride('_method'));
 app.use(flash());
-//seedDB();
+seedDB();
 
 
 app.use(require('express-session')({
